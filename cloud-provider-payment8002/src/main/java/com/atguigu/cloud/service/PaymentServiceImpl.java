ccb8a306d07aa53d87a57497e60079045d2fee3b
yan.km <43194876+dfjfj@users.noreply.github.com>
2023-08-12 17:37:20 +0800

    service方法增加事务注解

--- cloud-provider-payment8002/src/main/java/com/atguigu/cloud/service/PaymentServiceImpl.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/cloud/service/PaymentServiceImpl.java
@@ -4,6 +4,7 @@ import com.atguigu.cloud.api.PaymentService;
 import com.atguigu.cloud.api.dto.PaymentDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 业务处理层
@@ -14,11 +15,13 @@ public class PaymentServiceImpl implements PaymentService {
     @Autowired
     private PaymentRepository paymentRepository;
 
+    @Transactional
     @Override
     public int add(PaymentDTO paymentDTO) {
         return paymentRepository.insert(paymentDTO);
     }
 
+    @Transactional
     @Override
     public PaymentDTO getById(long id) {
         return paymentRepository.getById(id);
